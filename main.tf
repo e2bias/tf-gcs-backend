8eafc202211a842535f0455768b9523c69a87931
e2bias <70471970+e2bias@users.noreply.github.com>
2021-01-23 18:08:24 +0100

    use of uuid() for unique bucket names

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 # Create a GCS Bucket for remote state
 resource "google_storage_bucket" "tf-gcs-backend" {
-  name          = "${var.project_id}-${var.bucket_name}"
-  location      = var.location
+  name     = format("${var.bucket_name}-%s", uuid())
+  location = var.location
 
   versioning {
     enabled = true
   }
-  
+
   lifecycle_rule {
     condition {
       num_newer_versions = 10
--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "gcs-url" {
-  value = google_storage_bucket.tf-gcs-backend.url
+  value       = google_storage_bucket.tf-gcs-backend.url
   description = "The URL of the storage bucket (gs://)"
 }
